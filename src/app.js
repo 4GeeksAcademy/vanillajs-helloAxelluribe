d8fdedfd550b24cebf8b4f04b6d301f4016136a8
axelluribe <axelluribe13@gmail.com>
2025-08-08 19:31:43 +0000

    2 formas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-import "bootstrap";
-import "./style.css";
+letpronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+let domains = [];
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      domains.push(pronoun[p] + adj[a] + noun[n] + '.com');
+    }
+  }
+}
+
+console.log(domains);
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let  primero = ['El', 'nuestro'];
+let segundo  = ['genial','grande'];
+let tercero = ['Corredor','mapache'];
+
+let dominios = [];
+
+primero.forEach(p => {
+  segundo.forEach(s => {
+    tercero.forEach(t => {
+      dominios.push(p + s + t + '.com');
+    });
+  });
+});
+
+console.log(dominios);
